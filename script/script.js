7ccfaef25c3ac4d11621c56b69223b6c3dce9456
Lucas <imlucasmagalhaes@outlook.com>
2024-11-04 12:01:37 -0300

    main content for desktop finished

--- script/script.js
+++ script/script.js
@@ -4,11 +4,7 @@ mobileBtn.addEventListener('click', () => {
     const mobileMenu = document.querySelector('.mobile-menu')
     if(mobileMenu.classList.contains('active')){
         mobileMenu.classList.remove('active')
-        cross.classList.add('hidden')
-        burgerMenu.classList.remove('hidden')
     } else {
         mobileMenu.classList.add('active')
-        cross.classList.remove('hidden')
-        burgerMenu.classList.add('hidden')
     }
 })
